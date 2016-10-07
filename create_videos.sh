7b6a021250179b2d3e0c15ab5894e1ff0b3104bc
kedartatwawadi <ktatwawadi@gmail.com>
2016-10-06 22:12:07 -0700

    adding some more files

--- create_videos.sh
+++ create_videos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 frame_dir=~/Videos/leftImg8bit_sequence/val/frankfurt
-write_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x264_crf
+write_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x265_crf
 max_frames=1499
 
 cd $frame_dir
@@ -17,7 +17,7 @@ do
     for crf in `seq 0 8 32`
     do  
         mkdir -p $write_dir$crf
-        ffmpeg -r 17 -start_number $start_frame_num -i frankfurt_000000_%06d_leftImg8bit.png -vframes 30 -vcodec libx264 -pix_fmt yuv444p $write_dir$crf/frankfurt_000000_$start_frame_num.mp4    
+        ffmpeg -r 17 -start_number $start_frame_num -i frankfurt_000000_%06d_leftImg8bit.png -vframes 30 -vcodec libx265 -pix_fmt yuv444p $write_dir$crf/frankfurt_000000_$start_frame_num.mp4    
     done
 
 done
---
+++ run_obj_detect.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+val_dir=~/Videos/leftImg8bit/val
+base_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x264_crf
+code_path=/home/kedartatwawadi/code/video_compression/LRR/LRRTestOnCityScape.m
+log_path=/home/kedartatwawadi/Videos/leftImg8bit_sequence/cityscape_experiments/logs/log_obj_detect_exp_x264_crf
+
+cd $val_dir
+rm -r frankfurt/
+
+for crf in `seq 0 8 32`
+do
+    cp -r $base_dir$crf/frankfurt $val_dir
+    matlab -nodisplay -nodesktop -r "run $code_path" | tee -a $log_path$crf.txt 
+done
