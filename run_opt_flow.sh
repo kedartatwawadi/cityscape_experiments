46ca7ef34aed718b0c6f781623528e68c0f0801f
kedartatwawadi <ktatwawadi@gmail.com>
2016-10-07 06:56:30 -0700

    adding log files

--- run_opt_flow.sh
+++ run_opt_flow.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-base_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x264_crf
-crf0_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x264_crf0/optical_flow_frames
-log_path=/home/kedartatwawadi/Videos/leftImg8bit_sequence/cityscape_experiments/logs/log_opt_flow_exp_x264_crf
-code_path=/home/kedartatwawadi/Videos/leftImg8bit_sequence/cityscape_experiments/opt_flow_ked_2.py
+base_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x264_denoise_crf
+crf0_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x264_denoise_crf0/optical_flow_frames
+log_path=~/Videos/leftImg8bit_sequence/cityscape_experiments/logs/log_opt_flow_exp_x264_denoise_SF_crf
+code_path=~/Videos/leftImg8bit_sequence/cityscape_experiments/opt_flow_ked_3.py
 
-for crf in `seq 8 8 32`
+for crf in `seq 0 8 32`
 do
     cd $base_dir$crf/optical_flow_frames
     files=(*.png)
@@ -17,10 +17,10 @@ do
         #echo $f2
         f3=$crf0_dir/$f1
         f4=$crf0_dir/$f2
-        log_file=$log_path$crf
+        log_file=$log_path$crf.txt
         #echo $f3
         #echo $f4
         #echo $log_file
-        python $code_path $f1 $f2 $f3 $f4 $log_file$crf.txt
+        python $code_path $f1 $f2 $f3 $f4 $log_file
     done
 done
