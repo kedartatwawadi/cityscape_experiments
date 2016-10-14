f5cb8c16041451349e099cbd06d30f08924c9fa0
kedartatwawadi <ktatwawadi@gmail.com>
2016-10-13 23:05:59 -0700

    Adding some scripts to run DeepFLow

--- create_long_videos.sh
+++ create_long_videos.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#/bin/bash
 
-frame_dir=~/Videos/leftImg8bit_allFrames/val/frankfurt
-write_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x264_long_crf
-max_frames=1499
-nframes=1500
-cd $frame_dir
-files=(*.png)
-echo $frame_dir
+frame_dir=/media/kedar/New\ Volume/leftImg8bit_allFrames_frankfurt/leftImg8bit_allFrames/val/frankfurt
+write_dir=../../frankfurt_videos/x264_long_crf
+max_frames=22926
+nframes=5100
+cd /media/kedar/New\ Volume/leftImg8bit_allFrames_frankfurt/leftImg8bit_allFrames/val/frankfurt
+pwd
+files=(frankfurt_000000_*.png)
+echo ${files[0]}
+
 
 for iter in `seq 0 $nframes $max_frames`
 do
@@ -14,7 +16,7 @@ do
     echo $start_frame_name
     start_frame_num=$(echo $start_frame_name | sed 's/^frankfurt_000000_\([0-9]*\)_leftImg8bit.png$/\1/')
     echo $start_frame_num
-    for crf in `seq 0 8 32`
+    for crf in `seq 2 2 6`
     do  
         mkdir -p $write_dir$crf
         ffmpeg -r 17 -start_number $start_frame_num -i frankfurt_000000_%06d_leftImg8bit.png -vframes $nframes -vcodec libx264 -crf $crf -pix_fmt yuv444p $write_dir$crf/frankfurt_000000_$start_frame_num.mp4    
