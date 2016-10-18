97db7f3eee45aba2b6a9e1ad6934394f30ab7010
kedartatwawadi <ktatwawadi@gmail.com>
2016-10-18 13:30:56 -0700

    Adding vp9 opt flow log files

--- run_opt_flow.sh
+++ run_opt_flow.sh
@@ -3,21 +3,21 @@
 #base_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x265_crf
 #crf0_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x265_crf0/optical_flow_frames
 base_dir=/media/kedar/cfc96f03-783a-4353-be5f-c72a490aa372/kedar/frankfurt_videos/vp9_crf
-crf0_dir=/media/kedar/cfc96f03-783a-4353-be5f-c72a490aa372/kedar/frankfurt_videos/vp9_cr0/optical_flow_frames
+crf0_dir=/media/kedar/cfc96f03-783a-4353-be5f-c72a490aa372/kedar/frankfurt_videos/vp9_crf0/optical_flow_frames
 log_path=~/Videos/leftImg8bit_sequence/cityscape_experiments/logs/log_opt_flow_exp_vp9_crf
 code_path=~/Videos/leftImg8bit_sequence/cityscape_experiments/opt_flow_ked_2.py
 
-for crf in 0
+for crf in 0 2 4 6 8 16 24 32
 do
-    cd $base_dir$crf/optical_flow_frames
+    cd $base_dir$crf/optical_flow_frames/
     files=(*.png)
     
-    for iter in `seq 0 2 0`
+    for iter in `seq 0 2 99`
     do
         f1=${files[iter]}
         f2=${files[iter+1]}
-        echo $f1
-        echo $f2
+        #echo $f1
+        #echo $f2
         f3=$crf0_dir/$f1
         f4=$crf0_dir/$f2
         log_file=$log_path$crf.txt
@@ -25,9 +25,9 @@ do
 	#f1=$base_dir$crf/optical_flow_frames/$f1
 	#f2=$base_dir$crf/optical_flow_frames/$f2
 
-	echo $f3
-        echo $f4
-        echo $log_file
+	#echo $f3
+        #echo $f4
+        #echo $log_file
         python $code_path $f1 $f2 $f3 $f4 $log_file
     done
 done
