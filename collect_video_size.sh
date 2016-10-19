afafbd6efbd3a147729c20d59100793efce6d827
kedartatwawadi <ktatwawadi@gmail.com>
2016-10-18 22:31:12 -0700

    adding x264 file sizes

--- collect_video_size.sh
+++ collect_video_size.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #vid_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x265_crf
-vid_dir=/media/kedar/cfc96f03-783a-4353-be5f-c72a490aa372/kedar/frankfurt_videos/vp9_crf
-log_dir=~/Videos/leftImg8bit_sequence/cityscape_experiments/logs/log_vid_sizes_vp9_crf
+vid_dir=/media/kedar/cfc96f03-783a-4353-be5f-c72a490aa372/kedar/frankfurt_videos/x264_crf
+log_dir=~/Videos/leftImg8bit_sequence/cityscape_experiments/logs/log_vid_sizes_x264_crf
 
-for crf in 0 2 4 6 8 16 24 32
+for crf in 2 4 6
 do  
 	cd $vid_dir$crf
 	logfile=$log_dir$crf.txt
-	du *.webm > $logfile
+	du *.mp4 > $logfile
 done
