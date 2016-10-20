52b13fa19ae566e7bc83272a4e40ac74bcba17a1
kedartatwawadi <ktatwawadi@gmail.com>
2016-10-19 17:55:48 -0700

    adding x265 obj detect files

--- run_obj_detect.sh
+++ run_obj_detect.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 val_dir=~/Videos/leftImg8bit/val
-base_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/temp_folder_vp9/vp9_crf
+base_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/temp_folder_x265/x265_crf
 code_path=/home/kedartatwawadi/code/video_compression/LRR/LRRTestOnCityScape.m
-log_path=/home/kedartatwawadi/Videos/leftImg8bit_sequence/cityscape_experiments/logs/vp9_logs/log_obj_detect_exp_vp9_crf
+log_path=/home/kedartatwawadi/Videos/leftImg8bit_sequence/cityscape_experiments/logs/x265_logs/log_obj_detect_exp_x265_crf
 
 cd $val_dir
 rm -r frankfurt/
 
-for crf in 0 2 4 6 8 16 24 32
+for crf in 2 4 6
 do
     cp -r $base_dir$crf/frankfurt $val_dir
     matlab -nodisplay -nodesktop -r "run $code_path; quit" | tee -a $log_path$crf.txt 
