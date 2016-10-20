08079ac989d3ece74d6e78b33bb24a2a6529865d
kedartatwawadi <ktatwawadi@gmail.com>
2016-10-19 21:15:34 -0700

    adding modified script

--- run_obj_detect.sh
+++ run_obj_detect.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+source config.ini
 val_dir=~/Videos/leftImg8bit/val
-base_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/temp_folder_x265/x265_crf
+base_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/temp_folder_$codec/"$codec"_crf
 code_path=/home/kedartatwawadi/code/video_compression/LRR/LRRTestOnCityScape.m
-log_path=/home/kedartatwawadi/Videos/leftImg8bit_sequence/cityscape_experiments/logs/x265_logs/log_obj_detect_exp_x265_crf
+log_path=/home/kedartatwawadi/Videos/leftImg8bit_sequence/cityscape_experiments/logs/"$codec"_logs/log_obj_detect_exp_"$codec"_crf
 
 cd $val_dir
 rm -r frankfurt/
 
-for crf in 2 4 6
+for crf in $crf_list
 do
     cp -r $base_dir$crf/frankfurt $val_dir
     matlab -nodisplay -nodesktop -r "run $code_path; quit" | tee -a $log_path$crf.txt 
