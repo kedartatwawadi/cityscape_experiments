14c1a92191b966885b24f54d289f134beea81a7e
kedartatwawadi <ktatwawadi@gmail.com>
2016-11-02 18:09:08 -0700

    add file size log files

--- scripts/analysis_scripts/average_file_sizes.sh
+++ scripts/analysis_scripts/average_file_sizes.sh
@@ -6,6 +6,6 @@ log_dir=~/Videos/leftImg8bit_sequence/cityscape_experiments/logs/"$codec"_logs/
 for crf in $crf_list
 do  
 
-	logfile="$log_dir"log_vid_sizes_"$codec"_veryslow_crf"$crf".txt
+	logfile="$log_dir"log_vid_sizes_"$codec"_crf"$crf".txt
 	python avg_file_size.py $logfile #> "$log_dir"average_file_sizes_"$codec".txt
 done
--- scripts/analysis_scripts/collect_video_size.sh
+++ scripts/analysis_scripts/collect_video_size.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-source config.ini
+source ../config.ini
 #vid_dir=~/Videos/leftImg8bit_sequence/frankfurt_videos/x265_crf
-vid_dir=/media/kedar/cfc96f03-783a-4353-be5f-c72a490aa372/kedar/frankfurt_videos/"$codec"_veryslow_crf
-log_dir=~/Videos/leftImg8bit_sequence/cityscape_experiments/logs/"$codec"_logs/log_vid_sizes_"$codec"_veryslow_crf
+vid_dir=/media/kedar/cfc96f03-783a-4353-be5f-c72a490aa372/kedar/frankfurt_videos/"$codec"_crf
+log_dir=~/Videos/leftImg8bit_sequence/cityscape_experiments/logs/"$codec"_logs/log_vid_sizes_"$codec"_crf
+
 for crf in $crf_list
 do  
 	cd $vid_dir$crf
